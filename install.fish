18d49bad06286f9fc4b2e25c207aa1eb747c2847
fisiaque <fisiaque@gmail.com>
2025-09-12 20:43:05 +0100

    + fixed

--- install.fish
+++ install.fish
@@ -3,16 +3,142 @@
 argparse -n 'install.fish' -X 0 \
     'h/help' \
     'noconfirm' \
-    -- $args
+    'paru' \
+    -- $argv
 or exit
 
 # Print help
 if set -q _flag_h
-    echo 'usage: ./install.sh [-h] [--noconfirm]'
+    echo 'usage: ./install.sh [-h] [--noconfirm] [--paru]'
     echo
     echo 'options:'
     echo '  -h, --help                  show this help message and exit'
     echo '  --noconfirm                 do not confirm package installation'
+    echo '  --paru                      uses the aur helper paru instead of default yay'
 
     exit
 end
+
+# Functions
+function _out -a colour text
+    set_color $colour
+    # Pass arguments other than text to echo
+    echo $argv[3..] -- ":: $text"
+    set_color normal
+end
+
+function log -a text
+    _out cyan $text $argv[2..]
+end
+
+function input -a text
+    _out blue $text $argv[2..]
+end
+
+function confirm-overwrite -a path
+    if test -e $path -o -L $path
+        # No prompt if noconfirm
+        if set -q noconfirm
+            input "$path already exists. Overwrite? [Y/n]"
+            log 'Removing...'
+            rm -rf $path
+        else
+            # Prompt user
+            read -l -p "input '$path already exists. Overwrite? [Y/n] ' -n" confirm || exit 1
+
+            if test "$confirm" = 'n' -o "$confirm" = 'N'
+                log 'Skipping...'
+                return 1
+            else
+                log 'Removing...'
+                rm -rf $path
+            end
+        end
+    end
+
+    return 0
+end
+
+# Variables
+set -q _flag_noconfirm && set noconfirm '--noconfirm'
+set -q _flag_paru && set -l aur_helper paru || set -l aur_helper yay
+set -q XDG_CONFIG_HOME && set -l config $XDG_CONFIG_HOME || set -l config $HOME/.config
+set -q XDG_STATE_HOME && set -l state $XDG_STATE_HOME || set -l state $HOME/.local/state
+
+# Startup prompt
+set_color cyan
+echo '╭──────────────────────────────╮'
+echo '│   ____      __     __        │'
+echo '│  |  _ \   /\\ \   / //\       │'
+echo '│  | |_) | /  \\ \_/ //  \      │'
+echo '│  |  _ < / /\ \\   // /\ \     │'
+echo '│  | |_) / ____ \| |/ ____ \   │'
+echo '│  |____/_/    \_\_/_/    \_\  │'
+echo '│                              │'
+echo '╰──────────────────────────────╯'
+set_color normal
+
+log 'Welcome to the Baya dotfiles installer!'
+log 'Before continuing, please ensure you have made a backup of your config directory.'
+
+# Prompt for backup
+if ! set -q _flag_noconfirm
+    log '[1] No Need!  [2] Backup Needed!'
+    read -l -p "input '=> ' -n" choice || exit 1
+
+    if contains -- "$choice" 1 2
+        if test $choice = 2
+            log "Backing up $config..."
+
+            if test -e $config.bak -o -L $config.bak
+                read -l -p "input 'Backup already exists. Overwrite? [Y/n] ' -n" overwrite || exit 1
+
+                if test "$overwrite" = 'n' -o "$overwrite" = 'N'
+                    log 'Skipping...'
+                else
+                    rm -rf $config.bak
+                    cp -r $config $config.bak
+                end
+            else
+                cp -r $config $config.bak
+            end
+        end
+    else
+        log 'No choice selected. Exiting...'
+        exit 1
+    end
+end
+
+# Install AUR helper if not already installed
+if ! pacman -Q $aur_helper &> /dev/null
+    log "$aur_helper not installed. Installing..."
+
+    # Install
+    sudo pacman -S --needed git base-devel $noconfirm
+    cd /tmp
+    git clone https://aur.archlinux.org/$aur_helper.git
+    cd $aur_helper
+    makepkg -si
+    cd ..
+    rm -rf $aur_helper
+
+    # Setup
+    $aur_helper -Y --gendb
+    $aur_helper -Y --devel --save
+end
+
+# Install metapackage for deps
+log 'Installing metapackage...'
+$aur_helper -S --needed baya-meta $noconfirm
+
+# Cd into dir
+cd (dirname (status filename)) || exit 1
+
+# Install hypr* configs
+if confirm-overwrite $config/hypr
+    log 'Installing hypr* configs...'
+    ln -s (realpath configs/hypr) $config/hypr
+    hyprctl reload
+end
+
+log 'Baya Dots Finished Installing!'
