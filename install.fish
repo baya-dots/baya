cdc53786dbd52ae217a2567b9a8a2aac1f5c1fa5
fisiaque <fisiaque@gmail.com>
2025-09-12 18:36:52 +0100

    + base initialization

---
+++ install.fish
@@ -0,0 +1,18 @@
+#!/usr/bin/env fish
+
+argparse -n 'install.fish' -X 0 \
+    'h/help' \
+    'noconfirm' \
+    -- $argv
+or exit
+
+# Print help
+if set -q _flag_h
+    echo 'usage: ./install.sh [-h] [--noconfirm]'
+    echo
+    echo 'options:'
+    echo '  -h, --help                  show this help message and exit'
+    echo '  --noconfirm                 do not confirm package installation'
+
+    exit
+end
